1c71c5edd9230dbf6a4569911c7053bc00740de1
srinivasreddy-rr <rsrinivasr95@gmail.com>
2026-09-22 22:57:59 +0530

    Time: 5 ms (69.21%) | Memory: 160.1 MB (25.02%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,41 +1,33 @@
 class Solution {
     public int[] pivotArray(int[] arr, int k) {
         int n = arr.length;
-    int[] nums1 = new int[n];
-    int[] nums2 = new int[n];
-    int[] nums3 = new int[n];
-    int[] result = new int[n];
-    int x = 0;
-int y = 0;
-int z = 0;
-int p = 0;
-    for(int i = 0;i<n;i++){
-        if(arr[i] < k){
-            nums1[x] = arr[i];
-            x++;
+        int[] result = new int[n];
+        int x = 0;
+        int equal = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < k) {
+                x++;
+            } else if (arr[i] == k) {
+                equal++;
+            }
         }
-        else if(arr[i] > k){
-            nums2[y] = arr[i];
-            y++;
+        int l = 0;
+        int m = x;
+        int r = x + equal;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < k) {
+                result[l] = arr[i];
+                l++;
+            } else if (arr[i] > k) {
+                result[r] = arr[i];
+                r++;
+            } else {
+                result[m] = arr[i];
+                m++;
+            }
         }
-        else{
-         nums3[z] = arr[i];
-         z++;
-        }
-    }
-    for(int i = 0; i < x; i++){
-    result[p] = nums1[i];
-    p++;
-}
-for(int i = 0; i < z; i++){
-    result[p] = nums3[i];
-    p++;
-}
-for(int i = 0; i < y; i++){
-    result[p] = nums2[i];
-    p++;
-}
-return result;
+        return result;
 
     }
 }
